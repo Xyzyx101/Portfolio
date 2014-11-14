aaa5bd84c10883d31453d9efbaf4c7e1b1a34aa0
Xyzyx101 <Xyzyx101@notMyRealEmail.net>
2014-11-14 07:25:39 -0500

    HTML and JS done for nav bar.  CSS not done yet and touch screen version not done yet.

--- website_root/js/portfolio.js
+++ website_root/js/portfolio.js
@@ -1,8 +1,10 @@
-(function () {
+jQuery(document).ready(function($){
+
+    /* Resize sections to fill screen vertically */
     function calculateSectionSizes() {
         var vHeight = $(window).height();
-        var sectionContents = $('.section_content');
-        var sections = $('section');
+        var sectionContents = $(".section_content");
+        var sections = $(".nav_section");
 
         sectionContents.css({
             "position":"relative",
@@ -10,15 +12,57 @@
             "transform":"translateY(-50%)"
         });
         sections.css({
-            "height":vHeight,
-        })
+            "height":vHeight
+        });
     };
     calculateSectionSizes();
     $(window).resize(calculateSectionSizes);
-})();
 
-/*
-	position:relative;
-    top: 50%;
-	transform: 			translateY(-50%);
-*/
+    /* Nav bar */
+    var contentSections = $('.nav_section');
+	var navigationItems = $('#vertical_nav a');
+    console.log(navigationItems);
+
+    updateNavigation();
+	$(window).on('scroll', function(){
+		updateNavigation();
+	});
+    	//smooth scroll to the section
+	navigationItems.on('click', function(event){
+        event.preventDefault();
+        smoothScroll($(this.hash));
+    });
+    //smooth scroll to second section
+    /*TODO
+    $('.cd-scroll-down').on('click', function(event){
+        event.preventDefault();
+        smoothScroll($(this.hash));
+     });*/
+    //open-close navigation on touch devices
+    $('.touch .cd-nav-trigger').on('click', function(){
+    	$('.touch #cd-vertical-nav').toggleClass('open');
+
+    });
+    //close navigation on touch devices when selectin an elemnt from the list
+    $('.touch #cd-vertical-nav a').on('click', function(){
+    	$('.touch #cd-vertical-nav').removeClass('open');
+    });
+    function updateNavigation() {
+		contentSections.each(function(){
+			$this = $(this);
+			var activeSection = $('#vertical_nav a[href="#'+$this.attr('id')+'"]').data('number') - 1;
+			if ( ( $this.offset().top - $(window).height()/2 < $(window).scrollTop() ) && ( $this.offset().top + $this.height() - $(window).height()/2 > $(window).scrollTop() ) ) {
+				navigationItems.eq(activeSection).addClass('is-selected');
+			}else {
+				navigationItems.eq(activeSection).removeClass('is-selected');
+			}
+		});
+	}
+
+	function smoothScroll(target) {
+        $('body,html').animate(
+        	{'scrollTop':target.offset().top},
+        	600
+        );
+	}
+});
